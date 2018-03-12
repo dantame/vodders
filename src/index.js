d0d0fff76b8bede62b92f3876157b00a934bcde1
Dan Tame <dantame@Dans-MBP.lan>
2018-03-12 20:52:35 +0000

    add router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import {
+  Route,
+  Switch,
+} from 'react-router-dom'
+import { ConnectedRouter } from 'react-router-redux'
+
 
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import store from './store'
+import store, { history } from './store'
 
-ReactDOM.render((
-   <Provider store={store}>
-    <App />
-   </Provider>
-), document.getElementById('root'))
+class Index extends React.Component {
+  render() {
+    const { store, history } = this.props
+
+    return (
+       <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route path="/" component={App} />
+          </Switch>
+        </ConnectedRouter>
+       </Provider>
+    )
+  }
+}
+
+ReactDOM.render(<Index store={store} history={history} />, document.getElementById('root'))
 
 registerServiceWorker()
--- src/store.js
+++ src/store.js
@@ -1,14 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import createHistory from 'history/createBrowserHistory'
+import { routerMiddleware } from 'react-router-redux'
 
 import rootReducer from './reducers'
 import rootSaga from './sagas'
 
+export const history = createHistory()
+
 const sagaMiddleware = createSagaMiddleware()
+const routingMiddleware = routerMiddleware(history)
+
 const initialState = {}
 const enhancers = []
 const middleware = [
   sagaMiddleware,
+  routingMiddleware,
 ]
 
 if (process.env.NODE_ENV === 'development') {
